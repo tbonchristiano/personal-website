4fe6b01bca1503c4082f93cf35435c8f7152d003
tbonchristiano <122126722+tbonchristiano@users.noreply.github.com>
2023-01-06 18:24:51 -0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+$(function() {
+    $("#btn1").on("click", function(){
+        $("#about-me-txt").toggle()
+    });
+    $("#btn2").on("click", function(){
+        $("#work-exp-txt").toggle()
+    });
+    $("#btn3").on("click", function(){
+        $("#sports-txt").toggle()
+    });
+    $("#btn4").on("click", function(){
+        $("#education-txt").toggle()
+    });
+});
